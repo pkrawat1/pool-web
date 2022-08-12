1e66b2a2855cb8f347167c9eaee1a6c382baea09
pkrawat1 <pankajrawat19sept@gmail.com>
2022-08-12 17:27:20 +0530

    log fixes

--- components/common/token-logo.tsx
+++ components/common/token-logo.tsx
@@ -1,23 +1,30 @@
 import type { NextPage } from "next";
-import {IToken} from "@/types/";
+import { IToken } from "@/types/";
 import Image from "next/image";
+import { useState } from "react";
 import { tokenImgSrc } from "@/lib/";
 
 type Props = {
-  token: IToken,
-  width?: string,
-  height?: string,
-}
+  token: IToken;
+  width?: string;
+  height?: string;
+};
 
-const TokenLogo: NextPage<Props> = ({token, width, height}) => (
-    <span className="flex border rounded-full border-gray-500">
+const TokenLogo: NextPage<Props> = ({ token, width, height }) => {
+  const [src, setSrc] = useState(tokenImgSrc(token.id));
+
+  return (
+    <span className="flex border rounded-full border-gray-300">
       <Image
-        src={tokenImgSrc(token.id)}
+        src={src}
         alt={""}
         width={width || "18"}
         height={height || "18"}
+        className="rounded-full"
+        onError={() => setSrc('https://placehold.jp/32/ffffff/ff0000/40x40.jpg?text=%E2%9A%A0')}
       />
     </span>
-);
+  );
+};
 
 export default TokenLogo;
--- scripts/token-list.js
+++ scripts/token-list.js
@@ -1,24 +1,43 @@
 "use strict";
 
 const fs = require("fs");
-const https = require('https')
-
-const url = "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/tokenlist.json";
-
-https.get(url, res => {
-  let data = '';
-  res.on('data', chunk => {
-    data += chunk;
-  });
-  res.on('end', () => {
-    data = JSON.parse(data);
-    const tokenList = data.tokens.reduce((acc, token) => {
-      acc[token.address.toLowerCase()] = token.address;
-      return acc;
-    }, {});
-    console.log(tokenList);
-    fs.writeFileSync("./lib/token-list.json", JSON.stringify(tokenList));
-  })
-}).on('error', err => {
-  console.log(err.message);
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({
+  auth: "your token",
 });
+
+(async () => {
+  const res = await octokit.request('GET /repos/{owner}/{repo}/git/trees/{tree_sha}', {
+    owner: "trustwallet",
+      repo: "assets",
+      tree_sha: "b2b2108f7f86c7e22efdec6f446b35527be8f3d9",
+      truncated: true
+  })
+  // console.log(JSON.stringify(res))
+  const tokenIds = res.data.tree.reduce((acc, { path: fileName }) => {
+    acc[fileName.toLowerCase()] = fileName;
+    return acc;
+  }, {});
+  fs.writeFileSync("./lib/token-list.json", JSON.stringify(tokenIds));
+})();
+
+// https
+//   .get(options, (res) => {
+//     let data = "";
+//     res.on("data", (chunk) => {
+//       data += chunk;
+//     });
+//     res.on("end", () => {
+//       console.log(data);
+//       data = JSON.parse(data);
+//       const tokenList = data.tokens.reduce((acc, token) => {
+//         acc[token.address.toLowerCase()] = token.address;
+//         return acc;
+//       }, {});
+//       console.log(tokenList);
+//       fs.writeFileSync("./lib/token-list.json", JSON.stringify(tokenList));
+//     });
+//   })
+//   .on("error", (err) => {
+//     console.log(err.message);
+//   });
