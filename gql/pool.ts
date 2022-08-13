39d9b68c12885240717f63af204fbbf3459ca8b9
pkrawat1 <pankajrawat19sept@gmail.com>
2022-08-13 10:42:33 +0530

    refactor gql with common fragments

--- gql/pool.ts
+++ gql/pool.ts
@@ -1,58 +1,45 @@
 import { gql } from "@apollo/client";
 
+const POOL_COMMON_FIELDS = gql`
+  fragment PoolCommonFields on Pool {
+    id
+    txCount
+    totalValueLockedUSD
+    volumeUSD
+    token0 {
+      id
+      symbol
+    }
+    token1 {
+      id
+      symbol
+    }
+  }
+`;
+
 export const GET_POOLS_LIST = gql`
+  ${POOL_COMMON_FIELDS}
   query GetPoolsList($offset: Int!, $limit: Int!) {
     pools(skip: $offset, first: $limit) {
-      id
-      txCount
-      totalValueLockedUSD
-      volumeUSD
-      token0 {
-        id
-        symbol
-      }
-      token1 {
-        id
-        symbol
-      }
+      ...PoolCommonFields
     }
   }
 `;
 
 export const GET_SAVED_POOLS_LIST = gql`
+  ${POOL_COMMON_FIELDS}
   query GetPoolsList($ids: [String!], $offset: Int!, $limit: Int!) {
     pools(where: { id_in: $ids }, skip: $offset, first: $limit) {
-      id
-      txCount
-      totalValueLockedUSD
-      volumeUSD
-      token0 {
-        id
-        symbol
-      }
-      token1 {
-        id
-        symbol
-      }
+      ...PoolCommonFields
     }
   }
 `;
 
 export const GET_POOL_DETAILS = gql`
+  ${POOL_COMMON_FIELDS}
   query GetPoolDetails($id: String!) {
     pool(id: $id) {
-      id
-      txCount
-      totalValueLockedUSD
-      volumeUSD
-      token0 {
-        id
-        symbol
-      }
-      token1 {
-        id
-        symbol
-      }
+      ...PoolCommonFields
     }
   }
 `;
